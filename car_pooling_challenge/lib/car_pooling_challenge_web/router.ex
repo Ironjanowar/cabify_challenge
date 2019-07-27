289ae8f47e04cd746c48455f118962267a1aa36c
Ironjanowar <ironjanowar@gmail.com>
2019-07-27 15:32:48 +0200

    Two pipelines and deleted some comments

--- car_pooling_challenge/lib/car_pooling_challenge_web/router.ex
+++ car_pooling_challenge/lib/car_pooling_challenge_web/router.ex
@@ -1,16 +1,25 @@
 defmodule CarPoolingChallengeWeb.Router do
   use CarPoolingChallengeWeb, :router
 
-  pipeline :api do
-    plug(:accepts, ["json", "urlencoded"])
+  pipeline :api_json do
+    plug(:accepts, ["json"])
+  end
+
+  pipeline :api_urlencoded do
+    plug(:accepts, ["urlencoded", "json"])
   end
 
   scope "/", CarPoolingChallengeWeb do
-    pipe_through(:api)
+    pipe_through(:api_json)
 
     get("/status", StatusController, :status)
     put("/cars", CarsController, :set_cars)
     post("/journey", JourneyController, :journey)
+  end
+
+  scope "/", CarPoolingChallengeWeb do
+    pipe_through(:api_urlencoded)
+
     post("/dropoff", JourneyController, :dropoff)
     post("/locate", JourneyController, :locate)
   end
